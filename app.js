24ff98ce79ddf40c767d8c535bfa771d882652df
Simone823 <simone.daglio823@outlook.it>
2022-02-14 17:35:27 +0100

    aggiunta funzione verificoParolaPalindroma

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let userWord;
 // Finche la lunghezza del mio array word non è 1, chiederò all'utente di inserire una parola
 while (word.length !== 1){
     // Chiedo all'utente una parola
-    userWord = prompt("Inserisci una parola");
+    userWord = prompt("Inserisci una parola").trim().toLowerCase();
     console.log(userWord);
 
     if (!isNaN(userWord)){
@@ -17,21 +17,28 @@ while (word.length !== 1){
     }
 }
 
-// Splitto la parola inserita dall'utente e la inserisco in un array
-const wordSplit = [...userWord];
-console.log(wordSplit);
 
-// Inverto le parole dell'array wordSplit
-const wordInverted = wordSplit.reverse();
-console.log(wordInverted);
+function verificoParolaPalindroma (parola) {
+    
+    // Splitto la parola inserita dall'utente e la inserisco in un array
+    const wordSplit = [...parola];
+    console.log(wordSplit);
+    
+    // Inverto le parole dell'array wordSplit
+    const wordInverted = wordSplit.reverse();
+    console.log(wordInverted);
+    
+    // Unisco tutti gli elementi della stringa wordSplit per generare una stringa
+    const invertedJoin = wordInverted.join("");
+    console.log(invertedJoin);
+    
+    // Se la parola inserita dall'utente è uguale alla parola invertita, la parola è palindroma
+    if(userWord == invertedJoin){
+        console.log("palindroma");
+    } // Altrimenti la parola non è palindroma 
+        else {
+        console.log("non palindroma")
+    }
 
-// Unisco tutti gli elementi della stringa wordSplit per generare una stringa
-const invertedJoin = wordInverted.join("");
-console.log(invertedJoin);
-
-if(userWord == invertedJoin){
-    console.log("palindroma");
-} else {
-    console.log("non palindroma")
 }
-
+verificoParolaPalindroma(userWord);
